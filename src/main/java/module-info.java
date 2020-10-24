5bd6241690e31c955c824689ad3bd56b70852b7b
Juanan Pereira <juanan@ikasten.io>
2020-10-24 15:53:38 +0200

    ezarpenak sqlite (database out of resources folder)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,8 @@
 open module scenebuilder {
-  requires javafx.controls;
-  requires javafx.graphics;
-  requires javafx.fxml;
+
   requires java.sql;
-  requires org.jsoup;
-  requires com.google.gson;
-//  requires google.api.client;
-//  requires com.google.api.services.books;
-//  requires com.google.api.client.json.jackson2;
+  requires javafx.base;
+  requires javafx.fxml;
+  requires javafx.controls;
   exports ehu.isad;
 }
